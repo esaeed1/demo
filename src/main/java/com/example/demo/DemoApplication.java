17c3683c1d491a9d6810e2e822c31aa9a8ca839e
esaeed1 <edrees@wayne.edu>
2023-11-12 13:43:28 -0500

    hi

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/api")
+class MyController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, this is a sample endpoint!";
+	}
+}
